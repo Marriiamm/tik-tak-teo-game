0ff2be7e50faed44e068856bcd8cf5a439aa01d9
Marriiamm <marriiamm453@gmail.com>
2023-11-06 16:23:51 +0200

    UPDATES

--- lib/game.dart
+++ lib/game.dart
@@ -68,72 +68,55 @@ class Game {
       emptyCells.add(i);
     }
   }
-  if(ManagePlayer.playerX.containsAll(0, 1)&& emptyCells.contains(2)||
-     ManagePlayer.playerX.containsAll(4, 6)&& emptyCells.contains(2)||
-     ManagePlayer.playerX.containsAll(5, 8)&& emptyCells.contains(2)
-  ){index=2;}
-  if(ManagePlayer.playerO.containsAll(0, 1)&& emptyCells.contains(2)||
-     ManagePlayer.playerO.containsAll(4, 6)&& emptyCells.contains(2)||
-     ManagePlayer.playerO.containsAll(5, 8)&& emptyCells.contains(2)
-  ){index=2;}
-  else if(ManagePlayer.playerX.containsAll(1, 2)&& emptyCells.contains(0)||
-          ManagePlayer.playerX.containsAll(3, 6)&& emptyCells.contains(0)||
-          ManagePlayer.playerX.containsAll(4, 8)&& emptyCells.contains(0)
-  ){index=0;}
-  else if(ManagePlayer.playerO.containsAll(1, 2)&& emptyCells.contains(0)||
-          ManagePlayer.playerO.containsAll(3, 6)&& emptyCells.contains(0)||
-          ManagePlayer.playerO.containsAll(4, 8)&& emptyCells.contains(0)
-  ){index=0;}
-  else if(ManagePlayer.playerX.containsAll(0, 2)&& emptyCells.contains(1)||
-          ManagePlayer.playerX.containsAll(4, 7)&& emptyCells.contains(1)
-  ){index=1;}
-  else if(ManagePlayer.playerO.containsAll(0, 2)&& emptyCells.contains(1)||
-          ManagePlayer.playerO.containsAll(4, 7)&& emptyCells.contains(1)
-  ){index=1;}
-  else if(ManagePlayer.playerX.containsAll(4, 5)&& emptyCells.contains(3)||
-          ManagePlayer.playerX.containsAll(0, 6)&& emptyCells.contains(3)
-  ){index=3;}
-  else if(ManagePlayer.playerO.containsAll(4, 5)&& emptyCells.contains(3)||
-          ManagePlayer.playerO.containsAll(0, 6)&& emptyCells.contains(3)
-  ){index=3;}
-  else if(ManagePlayer.playerX.containsAll(0, 8)&& emptyCells.contains(4)||
-          ManagePlayer.playerX.containsAll(1, 7)&& emptyCells.contains(4)||
-          ManagePlayer.playerX.containsAll(2, 6)&& emptyCells.contains(4)||
-          ManagePlayer.playerX.containsAll(3, 5)&& emptyCells.contains(4)
-  ){index=4;}
-  else if(ManagePlayer.playerO.containsAll(0, 8)&& emptyCells.contains(4)||
-          ManagePlayer.playerO.containsAll(1, 7)&& emptyCells.contains(4)||
-          ManagePlayer.playerO.containsAll(2, 6)&& emptyCells.contains(4)||
-          ManagePlayer.playerO.containsAll(3, 5)&& emptyCells.contains(4)
-  ){index=4;}
-  else if(ManagePlayer.playerX.containsAll(2, 8)&& emptyCells.contains(5)||
-          ManagePlayer.playerX.containsAll(3, 4)&& emptyCells.contains(5)
-  ){index=5;}
-  else if(ManagePlayer.playerO.containsAll(2, 8)&& emptyCells.contains(5)||
-          ManagePlayer.playerO.containsAll(3, 4)&& emptyCells.contains(5)
-  ){index=5;}
-  else if(ManagePlayer.playerX.containsAll(2, 4)&& emptyCells.contains(6)||
-          ManagePlayer.playerX.containsAll(0, 3)&& emptyCells.contains(6)||
-          ManagePlayer.playerX.containsAll(7, 8)&& emptyCells.contains(6)
-  ){index=6;}
-  else if(ManagePlayer.playerO.containsAll(2, 4)&& emptyCells.contains(6)||
-          ManagePlayer.playerO.containsAll(0, 3)&& emptyCells.contains(6)||
-          ManagePlayer.playerO.containsAll(7, 8)&& emptyCells.contains(6)
-  ){index=6;}
-  else if(ManagePlayer.playerX.containsAll(1, 4)&& emptyCells.contains(7)||
-          ManagePlayer.playerX.containsAll(8, 6)&& emptyCells.contains(7)
-  ){index=7;}
-  else if(ManagePlayer.playerO.containsAll(1, 4)&& emptyCells.contains(7)||
-          ManagePlayer.playerO.containsAll(8, 6)&& emptyCells.contains(7)
-  ){index=7;}
-  else if(ManagePlayer.playerX.containsAll(0, 4)&& emptyCells.contains(8)||
-          ManagePlayer.playerX.containsAll(2, 5)&& emptyCells.contains(8)||
-          ManagePlayer.playerX.containsAll(6, 7)&& emptyCells.contains(8)
-  ){index=8;}
-  else if(ManagePlayer.playerO.containsAll(0, 4)&& emptyCells.contains(8)||
-          ManagePlayer.playerO.containsAll(2, 5)&& emptyCells.contains(8)||
-          ManagePlayer.playerO.containsAll(6, 7)&& emptyCells.contains(8)
-  ){index=8;}
+  if(ManagePlayer.playerO.containsAll(0, 1)&& emptyCells.contains(2))
+      {index=2;}
+  else if(ManagePlayer.playerO.containsAll(3, 4)&& emptyCells.contains(5))
+      {index=5;}
+  else if(ManagePlayer.playerO.containsAll(6, 7)&& emptyCells.contains(8))
+      {index=8;}
+  else if(ManagePlayer.playerO.containsAll(0, 3)&& emptyCells.contains(6))
+      {index=6;}
+  else if(ManagePlayer.playerO.containsAll(1, 4)&& emptyCells.contains(7))
+      {index=7;}
+  else if(ManagePlayer.playerO.containsAll(2, 5)&& emptyCells.contains(8))
+      {index=8;}
+  else if(ManagePlayer.playerO.containsAll(2, 4)&& emptyCells.contains(5))
+      {index=6;}
+  else if(ManagePlayer.playerO.containsAll(0, 2)&& emptyCells.contains(1))
+      {index=1;}
+  else if(ManagePlayer.playerO.containsAll(3, 5)&& emptyCells.contains(4))
+      {index=4;}
+  else if(ManagePlayer.playerO.containsAll(6, 8)&& emptyCells.contains(7))
+      {index=7;}
+  else if(ManagePlayer.playerO.containsAll(0, 6)&& emptyCells.contains(3))
+      {index=3;}
+  else if(ManagePlayer.playerO.containsAll(1, 7)&& emptyCells.contains(4))
+      {index=4;}
+  else if(ManagePlayer.playerO.containsAll(2, 8)&& emptyCells.contains(5))
+      {index=5;}
+  else if(ManagePlayer.playerO.containsAll(0, 8)&& emptyCells.contains(4))
+      {index=4;}
+  else if(ManagePlayer.playerO.containsAll(2, 6)&& emptyCells.contains(4))
+      {index=4;}
+
+
+
+  else if(ManagePlayer.playerO.containsAll(1, 2)&& emptyCells.contains(0))
+      {index=0;}
+  else if(ManagePlayer.playerO.containsAll(4, 5)&& emptyCells.contains(3))
+      {index=3;}
+  else if(ManagePlayer.playerO.containsAll(7, 8)&& emptyCells.contains(6))
+      {index=6;}
+  else if(ManagePlayer.playerO.containsAll(3, 6)&& emptyCells.contains(0))
+      {index=0;}
+  else if(ManagePlayer.playerO.containsAll(4, 7)&& emptyCells.contains(1))
+      {index=1;}
+  else if(ManagePlayer.playerO.containsAll(5, 8)&& emptyCells.contains(2))
+      {index=2;}
+  else if(ManagePlayer.playerO.containsAll(4, 8)&& emptyCells.contains(0))
+      {index=0;}
+  else if(ManagePlayer.playerO.containsAll(4, 6)&& emptyCells.contains(2))
+      {index=2;}
   else
   {
   Random randon =Random();
--- lib/views/start_screen.dart
+++ lib/views/start_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tik_tak_game/views/home.dart';
+import 'package:tik_tak_game/widgets/start_button.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -27,39 +27,3 @@ class StartScreen extends StatelessWidget {
 }
 
 
-class ButtonOne extends StatelessWidget {
-  const ButtonOne({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      style: ButtonStyle(
-        maximumSize: MaterialStateProperty.all<Size>(const Size(140, 50)),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50),
-          ),),
-        backgroundColor: MaterialStateProperty.all(const Color(0xffF4E869)),
-      ),
-      onPressed: (){
-        Navigator.push(context, MaterialPageRoute(
-          builder: (context){
-            return  const HomePage();
-          }));
-      }, 
-       
-      label: const Text("Start",
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 17,
-        color: Color(0xff443680),
-        fontFamily: 'Exo2',
-      ),),
-      icon: const Icon(Icons.arrow_left_rounded,
-      color: Color(0xff443680),
-      size: 50,),
-          );
-  }
-}
